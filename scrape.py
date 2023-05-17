6b7107261a359804f55dbdf0156daad9ba6a38d5
Archer Taylor <wataylor1@crimson.ua.edu>
2023-05-16 20:45:37 -0700

    Removes tests from scrape.py

--- scrape.py
+++ scrape.py
@@ -16,9 +16,5 @@ def scrape_course(course_prefix, course_number):
     course_info["Credit Hours"] = td_element.find("br").next_element.strip().replace("Credit hours", "")
     course_info["Levels"] = td_element.find("span", class_="fieldlabeltext", text="Levels: ").next_sibling.strip()
     course_info["Schedule Types"] = td_element.find("span", class_="fieldlabeltext", text="Schedule Types: ").next_sibling.strip()
-
-    print(course_info["Schedule Types"])
     
     return course_info
-
-scrape_course("CS", "407")

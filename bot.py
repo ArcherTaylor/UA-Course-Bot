b13df5114e7f164a7aeb79b7c57d7717911dc290
Archer Taylor <wataylor1@crimson.ua.edu>
2023-05-18 20:03:20 -0700

    Updates scrape and bot

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
-# bot.py
 import os
 from dotenv import load_dotenv
 
-# 1
 import discord
 import scrape
 from discord import app_commands
@@ -26,6 +24,7 @@ async def on_ready():
 @bot.tree.command(name="course")
 @app_commands.describe(course_prefix = "EX: CS, ENGR, AEM, etc...", course_number = "What is the number of the course?")
 async def say(interaction: discord.Interaction, course_prefix: str, course_number: str):
+    course_prefix = course_prefix.upper()
     course_info = scrape.scrape_course(course_prefix, course_number)
     if course_info is None:
         embed=discord.Embed(title=f"{course_prefix} {course_number}", url=f"https://ssb.ua.edu/pls/PROD/bwckctlg.p_disp_course_detail?cat_term_in=202340&subj_code_in={course_prefix}&crse_numb_in={course_number}", description="No course found.", color=0x9e1b32)
@@ -35,6 +34,8 @@ async def say(interaction: discord.Interaction, course_prefix: str, course_numbe
         embed.add_field(name="Credit Hours", value=course_info["Credit Hours"])
         embed.add_field(name="Level", value=course_info["Levels"])
         embed.add_field(name="Type", value=course_info["Schedule Types"])
+        if course_info["Prerequisites"] != None:
+            embed.add_field(name="Prerequisites", value=course_info["Prerequisites"], inline=False)
     await interaction.response.send_message(embed=embed)
 
 bot.run(TOKEN)
--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def scrape_course(course_prefix, course_number):
     course_info = {}
@@ -7,14 +8,48 @@ def scrape_course(course_prefix, course_number):
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, "html.parser")
+
+    # On a valid course, the information will always be contained in the first ntdefault class
     td_element = soup.find("td", class_="ntdefault")
 
+    # Based on the pattern of UA's website, if no course is found then this will be the first "ntdefault" class found
     if "Return to Previous" in td_element.text:
         return None
 
+    # Finds the course description, credit hours, and levels based off of the pattern of UA's website
     course_info["Description"] = td_element.find("br").previous_element.strip()
     course_info["Credit Hours"] = td_element.find("br").next_element.strip().replace("Credit hours", "")
     course_info["Levels"] = td_element.find("span", class_="fieldlabeltext", text="Levels: ").next_sibling.strip()
-    course_info["Schedule Types"] = td_element.find("span", class_="fieldlabeltext", text="Schedule Types: ").next_sibling.strip()
-    
+
+    # Navigates through UA's website and strips away all the <a> tags from the schedule types (if they have one)
+    schedule_types = td_element.find("span", class_="fieldlabeltext", text="Schedule Types: ")
+    schedule_types_result = ''
+    for sibling in schedule_types.next_siblings:
+        if sibling.name == "br":
+            break
+        schedule_types_result += sibling.get_text()
+    course_info["Schedule Types"] = schedule_types_result.strip()
+
+    # Get's the raw text for prerequisites from UA's website
+    prerequisites = td_element.find("span", class_="fieldlabeltext", text="Prerequisites: ")
+    if prerequisites is None:
+        course_info["Prerequisites"] = None
+    else:
+        prerequisites_result = ''
+        for sibling in prerequisites.next_sibling.next_sibling.next_siblings:
+            if sibling.name == "br":
+                break
+            prerequisites_result += sibling.get_text()
+
+        # Uses regular expression to remove additional information that will not be included
+        pattern = r'(?:(?:Undergraduate|Graduate)\s?level\s\s?)|\(Concurrent Enrollment Allowed\)\s*|\b\s?Minimum Grade of [A-F][\+-]?'
+        prerequisites_result = re.sub(pattern, '', prerequisites_result)
+        prerequisites_result = re.sub(r'\s{2,}', ' ', prerequisites_result)
+        
+        course_info["Prerequisites"] = prerequisites_result.strip()
+
+        print(course_info["Prerequisites"])
+
     return course_info
+
+scrape_course("ENGR", "123")
